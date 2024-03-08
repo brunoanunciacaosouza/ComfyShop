21a43d62f1c47d26a8038f19d83491d150324dbf
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-03-08 12:20:58 -0300

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  SingleProduct,
+  Register,
+} from './pages';
+
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
