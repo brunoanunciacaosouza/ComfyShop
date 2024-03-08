73df4f76f039b09d0971b168fa017d72596a4fea
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-03-08 12:40:18 -0300

    route structure

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
 const HomeLayout = () => {
   return (
-    <div>HomeLayout</div>
-  )
-}
+    <>
+      <nav>
+        <span className="text-4xl text-primary">Comfy</span>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
