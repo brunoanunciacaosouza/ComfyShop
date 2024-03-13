f73482d5a81e74975784a2d8961cb8be9b1f1a4f
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-03-13 16:59:18 -0300

    update Filter component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,40 +3,39 @@ import FormInput from './FormInput';
 import FormSelect from './FormSelect';
 import FormRange from './FormRange';
 
-
 const Filters = () => {
   const { meta } = useLoaderData();
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-       <FormInput
-        type='search'
-        label='search product'
-        name='search'
-        size='input-sm'
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        size="input-sm"
       />
       <FormSelect
-        label='select category'
-        name='category'
+        label="select category"
+        name="category"
         list={meta.categories}
-        size='select-sm'
+        size="select-sm"
       />
       <FormSelect
-        label='select company'
-        name='company'
+        label="select company"
+        name="company"
         list={meta.companies}
-        size='select-sm'
+        size="select-sm"
       />
       <FormSelect
-        label='sort by'
-        name='order'
+        label="sort by"
+        name="order"
         list={['a-z', 'z-a', 'high', 'low']}
-        size='select-sm'
+        size="select-sm"
       />
-      <FormRange name="price" label="select price" size="range-sm"/>
-       <button type='submit' className='btn btn-primary btn-sm'>
+      <FormRange name="price" label="select price" size="range-sm" />
+      <button type="submit" className="btn btn-primary btn-sm">
         search
       </button>
-      <Link to='/products' className='btn btn-accent btn-sm'>
+      <Link to="/products" className="btn btn-accent btn-sm">
         reset
       </Link>
     </Form>
