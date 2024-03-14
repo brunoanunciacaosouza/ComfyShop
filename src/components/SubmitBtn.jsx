aff59ab2e41f124edaf6490bdbca8050d130db2c
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-03-14 15:26:35 -0300

    add logout and access user functionality

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -3,8 +3,6 @@ import { useNavigation } from 'react-router-dom';
 const SubmitBtn = ({ text }) => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
-
-  console.log(navigation);
   return (
     <button
       type="submit"
