caa13869ffc11596a86fb1cc91ca7baeb66f396f
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-03-18 13:58:52 -0300

    add react query - single product page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -5,10 +5,20 @@ import { useState } from 'react';
 
 import { addItem } from '../features/cart/cartSlice';
 
+const singleProductQuery = (id) => {
+  return {
+    queryKey: ['singleProduct', id],
+    queryFn: () => customFetch(`/products/${id}`),
+  };
+};
+
 export const loader =
   (queryClient) =>
   async ({ params }) => {
-    const response = await customFetch(`/products/${params.id}`);
+    const response = await queryClient.ensureQueryData(
+      singleProductQuery(params.id)
+    );
+
     return { product: response.data.data };
   };
 
